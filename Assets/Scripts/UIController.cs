753cb4aa3c09abad596d8497cf3be84077ae15b4
Hikaru <h.s@hs.local>
2017-05-04 15:36:11 +0900

    add sniper function

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UIController : MonoBehaviour {
-	[SerializeField] private GameObject player;
 	[SerializeField] private GUIStyle guiStyle;
 	[SerializeField] private Texture reticle;
+	[SerializeField] private Texture snipe;
 	private RayController rayController;
 	private WeaponController weaponController;
 	private float time;
-	// Use this for initialization
+	private Camera playerCamera;
+
 	void Start () {
-		rayController =player.GetComponent<RayController>();
-		weaponController = player.GetComponent<WeaponController>();
+		rayController = GetComponent<RayController>();
+		weaponController = GetComponent<WeaponController>();
+		playerCamera = GetComponent<Camera> ();
 	}
 	
 	// Update is called once per frame
@@ -23,13 +25,22 @@ public class UIController : MonoBehaviour {
 	void OnGUI (){
 		Rect position1 = new Rect (10, Screen.height - 40, 200, 40);
 		Rect position2 = new Rect (10, Screen.height - 20, 200, 40);
-		Rect position3 = new Rect (Screen.width - 130, Screen.height - 40, 200, 40);			Rect position4 = new Rect (Screen.width - 130, Screen.height - 20, 200, 40);
-		Rect positioncenter = new Rect (Screen.width/2 - 20, Screen.height/2 - 20, 40, 40);
-	
+		Rect position3 = new Rect (Screen.width - 130, Screen.height - 40, 200, 40);
+		Rect position4 = new Rect (Screen.width - 130, Screen.height - 20, 200, 40);
+		Rect positioncenter = new Rect (Screen.width / 2 - 20, Screen.height / 2 - 20, 40, 40);
+		Rect positionsnipe = new Rect (Screen.width / 2 - 750, Screen.height / 2 - 750, 1500, 1500);
+
 		GUI.Label (position1, "Time:" + time + "s", guiStyle);
 		GUI.Label (position2, "Pt:" + rayController.score, guiStyle);
 		GUI.Label (position3, "BulletBox:" + weaponController.bulletbox, guiStyle);
 		GUI.Label (position4, "Bullet:" + weaponController.currentbullet + "/" + weaponController.maxbullet, guiStyle);
 		GUI.DrawTexture (positioncenter, reticle);
+		if (Input.GetMouseButton (1)) {
+			GUI.DrawTexture (positionsnipe, snipe);
+			playerCamera.fieldOfView = 5;
+		}
+		if (Input.GetMouseButtonUp (1)) {
+			playerCamera.fieldOfView = 60;
+		}
 	}
 }
